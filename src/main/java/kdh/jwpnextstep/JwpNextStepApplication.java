2545166c6d1c82d398dc235a0352a2e973532677
Hwan0518 <qip0518@naver.com>
2023-12-05 14:30:16 +0900

    init: first commit for 자바 웹 프로그래밍 Next Step

---
+++ src/main/java/kdh/jwpnextstep/JwpNextStepApplication.java
@@ -0,0 +1,13 @@
+package kdh.jwpnextstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpNextStepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwpNextStepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kdh/jwpnextstep/JwpNextStepApplicationTests.java
@@ -0,0 +1,13 @@
+package kdh.jwpnextstep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwpNextStepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
